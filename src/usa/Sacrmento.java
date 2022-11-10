9e28810a144422eb17d83283f5b4634d157e0b11
SaidIqbaSadat <117493288+SaidIqbaSadat@users.noreply.github.com>
2022-11-10 09:18:04 -0800

    Update Sacrmento.java
    just an small correction

--- src/usa/Sacrmento.java
+++ src/usa/Sacrmento.java
@@ -3,6 +3,7 @@ package usa;
 public class Sacrmento {
 	public static void main(String[] args) {
 		System.out.println("this is inside world branch");
+		System.out.println("don't think on everything");
 	}
 
 }
